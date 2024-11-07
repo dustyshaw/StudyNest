4492007d17343b08412cd70f662b386c4d1ea564
Dusty Shaw <shustydaw@gmail.com>
2024-11-06 20:54:37 -0700

    can log in

---
+++ studynest/src/LoginBtn.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
+
+
+function LoginButton() {
+  const auth = useAuth();
+
+    useEffect(() => {
+        console.log("NEw yser");
+        if (auth.user) {
+            const date = new Date((auth.user.expires_at ?? 0)* 1000)
+            document.cookie = `auth_token=${auth.user.id_token};expires=${date.toUTCString()}`
+        }
+    })
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        Hello {auth.user?.profile.sub}{" "}
+        <button onClick={() => {void auth.removeUser(); auth.removeUser()}}>Log out</button>
+      </div>
+    );
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+}
+
+export default LoginButton;
--- studynest/src/main.tsx
+++ studynest/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 
-createRoot(document.getElementById('root')!).render(
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "studynest-authclient",
+  redirect_uri: "http://localhost:5173/",
+  onSigninCallback: async (user) => {
+    console.log("USER TOKEN ----------", user?.id_token);
+    document.cookie = `jwt_token=${user?.id_token}`;
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+  automaticSilentRenew: true,
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
