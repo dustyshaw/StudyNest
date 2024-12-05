7f2e3d9924bc4c172148ec22596d21cfc4b0121b
Dusty Shaw <shustydaw@gmail.com>
2024-12-04 20:53:59 -0700

    added back button

--- studynest/src/components/Task/Task.tsx
+++ studynest/src/components/Task/Task.tsx
@@ -6,6 +6,7 @@ import "./DatePicker.modules.css";
 import LogHours from "./LogHours";
 import SecondaryBtn from "../LayoutComponents/SecondaryBtn";
 import CompleteTask from "./CompleteTask";
+import BackButton from "../LayoutComponents/BackButton";
 
 const ViewTask = () => {
   const { taskId } = useParams();
@@ -16,29 +17,25 @@ const ViewTask = () => {
   }
 
   return (
-    <div className="">
-      <p className="mb-3 m-8 text-gray-600 font-semibold">
-        <Link to={"/"}>Dashboard</Link> / Unit / {task.title}
-      </p>
-      <div className="m-8">
-        <h1 className="text-2xl">{task.title}</h1>
-        <p>Due at {formatDate(task.duedate)}</p>
-        <CompleteTask task={task} />
-        <div className="flex flex-row">
-          <div className=" bg-gray-50 rounded-lg p-8 w-full">
-            {task?.description}
-          </div>
-          <div className="w-1/3 mx-4 p-8 bg-gray-50 rounded-lg">
-            <p>
-              {task.hours} Hrs and {task.minutes} min spent on this task!
-            </p>
-            <LogHours taskId={task.id} />
-          </div>
+    <div className="m-8">
+      <BackButton />
+      <h1 className="text-2xl">{task.title}</h1>
+      <p>Due at {formatDate(task.duedate)}</p>
+      <CompleteTask task={task} />
+      <div className="flex flex-row">
+        <div className=" bg-gray-50 rounded-lg p-8 w-full">
+          {task?.description}
         </div>
-        <Link to={`/task/edit/${taskId}`}>
-          <SecondaryBtn>Edit Task</SecondaryBtn>
-        </Link>
+        <div className="w-1/3 mx-4 p-8 bg-gray-50 rounded-lg">
+          <p>
+            {task.hours} Hrs and {task.minutes} min spent on this task!
+          </p>
+          <LogHours taskId={task.id} />
+        </div>
       </div>
+      <Link to={`/task/edit/${taskId}`}>
+        <SecondaryBtn>Edit Task</SecondaryBtn>
+      </Link>
     </div>
   );
 };
